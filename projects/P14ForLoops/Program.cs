24d8554b313176c4e52e0ce57b88c7fd9dfe69b2
jackahlberg <ahlberg.jack@gmail.com>
2022-01-31 16:14:17 +0100

    Writes all code for P14Loops

--- projects/P14ForLoops/Program.cs
+++ projects/P14ForLoops/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 200; i > 99; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1024; i > 0 ; i /= 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
