d8c5d52ff6690017beb9d63c5dbb00942f22865b
jackahlberg <ahlberg.jack@gmail.com>
2022-02-14 14:26:34 +0100

    Writes code for array-assignment

--- projects/P10ObjectOrientedProgramming/Program.cs
+++ projects/P10ObjectOrientedProgramming/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static string[] names = new string[3];
+    static void Main()
+    {
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            names[i] = Console.ReadLine();
+        }
+        
+        Console.Write(names[0] + ", " + names[1] + ", " + names[2]);
+    }
+}
