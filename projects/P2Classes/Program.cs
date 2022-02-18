1fe33a2ddbca34646ea3e48723c159d98929ee57
jackahlberg <ahlberg.jack@gmail.com>
2022-02-18 13:57:59 +0100

    Finishes all code for P2Classes

---
+++ projects/P2Classes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Classes;
+
+
+namespace Classes
+{
+    public class Person
+    {
+        public string name;
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hello, my name is {name}");
+        }
+    }   
+    
+
+class Program
+{
+    static void Main()
+    {
+        Person human = new Person();
+        Animal dog = new Animal();
+        Car volvo = new Car();
+            
+        Console.WriteLine(human);
+        Console.WriteLine(dog);
+        Console.WriteLine(volvo);
+            
+
+        Person[] persons = new Person[3];
+            
+            
+        for (int i = 0; i < persons.Length; i++)
+        {
+            persons[i] = new Person();
+            Console.WriteLine("Give me a name.");
+            persons[i].name = Console.ReadLine();
+                
+        }
+
+        for (int i = 0; i < persons.Length; i++)
+        {
+            persons[i].IntroduceYourself();
+        }
+
+    }
+}
+
+
+    class Animal
+    {
+        public static Animal dog = new Animal();
+    }
+
+    class Car
+    {
+        public static Car Volvo = new Car();
+    }
+    
+
+}
+
